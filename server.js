a93eb3bbee3d88ea9baf01925ad2c3b376ef7688
Ruben <rcbruijn1@avans.nl>
2017-06-16 18:14:28 +0200

    heroku test 3

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ var app = express();
 
 app.use('/api/v1', routes_v1);
 
-app.set('port', (process.env.PORT | config.webPort));
-app.set('env', (process.env.ENV | 'development'))
-
-
 
 app.listen(process.env.PORT ||8080, function () {
     console.log('De server luistert op port 8080');
