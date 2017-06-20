faa1ffc1e94100b8db0fbd5b6fc455936be30e53
Ruben <rcbruijn1@avans.nl>
2017-06-20 12:23:51 +0200

    last commit, final version development

--- db.js
+++ db.js
@@ -4,8 +4,8 @@
 var mysql = require('mysql');
 var config = require('./config');
 
-var pool  = null;
-exports.connect = function() {
+var pool = null;
+exports.connect = function () {
     pool = mysql.createPool({
         host: process.env.DB_HOST || config.dbHost,
         user: process.env.DB_USER || config.dbUser,
@@ -13,6 +13,6 @@ exports.connect = function() {
         database: process.env.DB_DATABASE || config.dbDatabase
     });
 }
-exports.get = function() {
+exports.get = function () {
     return pool;
 }
